ae3dfc0c6540bb78e4bc87374157f73af31c6e41
dylan <dylan>
2021-06-10 10:09:47 -0500

    database1

--- Database.php
+++ Database.php
@@ -14,6 +14,10 @@ class Database extends PDO{
 	private $port 	= 5432;
     //instance
 	private $dbh;
+
+	$strCnx = "host=$host port=$port dbname=$db user=$user password=$passwd";
+	$cnx = pg_connect($strCnx) or die ("Error de conexion. ". pg_last_error());
+	echo "Conexion exitosa <hr>";
  
 	//connect with postgresql and pdo
 	public function __construct(){
