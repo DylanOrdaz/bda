141fbf5f512f8be0d8acc8a35f59c154b2be7012
dylan <dylan>
2021-06-10 09:51:54 -0500

    Registro pacientes

--- tablePacientes.php
+++ tablePacientes.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>Consulta de citas entre dos fechas</title>
+  <title>Registro de pacientes</title>
   <link rel="icon" href="img/hospital.ico" type="image/x-icon">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
@@ -13,61 +13,93 @@
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body class="bg">
-  <div class="d-flex justify-content-center align-items-center text-brown">
-    <form class="mt-3 needs-validation" action="tablePacientesBusqueda.php" method="post" novalidate>
-      <h3 class="font-weight-bold">Busqueda de pacientes</h3>
-      <div class="mt-4">
-      <div class="form-row">
-        <div class="form-group col-md-12">
-          <label for="filtro">Selecciona el tipo de busqueda</label>
-          <select class="browser-default custom-select" id="filtro" name="filtro" required>
-            <option selected value="">Selecciona tipo de busqueda</option>
-            <option value="idPaciente">Id</option>
-            <option value="nombre">Nombre del paciente</option>
-            <option value="apellidoP">Apellido Paterno</option>
-            <option value="celular">Numero de celular</option>
-            <option value="email">E-mail</option>
-            <option value="localidad">Localidad</option>
-            </select>
+  <div class="container mt-3">
+    <h3 class="font-weight-bold">Registro de pacientes</h3>
+    <br>
+      <div class="row col-md-12">
+      <a href="index.html" class="btn btn-default btn-md">Regresar</a>
+      </div>
+      <table class="table table-bordered table-light">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">Nombre (s)</th>
+            <th scope="col">Apellidos</th>
+            <th scope="col">Celular</th>
+            <th scope="col">E-mail</th>
+            <th scope="col">Localidad</th>
+            <th scope="col">Edad</th>
+            <th scope="col">Opciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php 
+                include 'database.php';
+                include 'funcs.php';
+                $pdo = Database::connect();
+                $sql = 'SELECT idPaciente, nombre, CONCAT (apellidoP," ", apellidoM) as apellidos, celular, email, localidad, fechaNacimiento from paciente order by idPaciente;';
+                foreach ($pdo->query($sql) as $row) {
+                echo '<tr>';                  
+                    echo '<td>'. $row['nombre'] . '</td>';
+                    echo '<td>'. $row['apellidos'] . '</td>';
+                    echo '<td>'. $row['celular'] . '</td>';
+                    echo '<td>'. $row['email'] . '</td>';
+                    echo '<td>'. $row['localidad'] . '</td>';
+                    echo '<td>'. calculaedad($row['fechaNacimiento']) . '</td>';
+                    echo '<td width=390>';
+                    //echo '<div class="btn-toolbar" role="group">';
+                    echo '<a class="btn btn-outline-info btn-sm" onClick="loadDynamicContentModal('.$row['idPaciente'].')">Cuadro Clinico</a>';                    
+                    echo '&nbsp;';
+                    echo '<a class="btn btn-outline-light-green btn-sm" href="actPaciente.php?id='.$row['idPaciente'].'">Actualizar</a>';                    
+                    echo '&nbsp;';
+                    echo '<a class="btn btn-outline-danger btn-sm" href="deletePaciente.php?id='.$row['idPaciente'].'">Eliminar</a>';
+                    //echo '</div>';
+                    echo '</td>';
+                echo '</tr>';
+                }
+                Database::disconnect();
+                ?>                  
+        </tbody>
+      </table>
+      <div class="row col-md-12">
+      <a href="index.html" class="btn btn-default btn-md">Regresar</a>
+      <a href="regPaciente.php" class="btn btn-default btn-md ml-auto">Registra un paciente</a>
+      </div>
+      <!-- Modal----------------------------------------------------------------------------------->
+      <div class="modal fade" id="bootstrap-modal" role="dialog">
+        <div class="modal-dialog" role="document"> 
+          <!-- Modal contenido-->
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Cuadro clinico del paciente</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
+            </div>
+            <div class="modal-body">
+              <div id="conte-modal"></div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-elegant" data-dismiss="modal">Cerrar</button>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="form-row">
-        <div class="form-group col-md-12">
-          <label for="valor">Valor</label>
-          <input placeholder="Introduce el valor de tu busqueda" type="text" name="valor" id="valor" class="form-control" required>
-        </div>
-      </div>
-       <div class="form-row col-md-12">
-        <a href="index.html" class="btn btn-default btn-md">Regresar</a>
-        <button type="submit" class="btn btn-primary btn-md ml-auto">Buscar</button>
-       </div>
-       </div>
-     </form>
+    <!-- ------------------------------------------------------------------------------------>
   </div>
   <script type="text/javascript" src="js/jquery.min.js"></script>
   <script type="text/javascript" src="js/popper.min.js"></script>
   <script type="text/javascript" src="js/bootstrap.min.js"></script>
   <script type="text/javascript" src="js/mdb.min.js"></script>
   <script type="text/javascript">
-    (function() {
-        'use strict';
-        window.addEventListener('load', function() {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-        }, false);
-        });
-        }, false);
-})();
-
+    function loadDynamicContentModal(modal){
+  var options = {
+      modal: true,
+      height:300,
+      width:600
+    };
+  $('#conte-modal').load('datosModalPaciente.php?my_modal='+modal, function() {
+    $('#bootstrap-modal').modal({show:true});
+    });    
+}
   </script>
-
 </body>
 </html>
+
