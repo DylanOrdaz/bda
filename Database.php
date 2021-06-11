a4cf4c375623be41e0eeb01b6389dd56bcea174a
dylan <dylan>
2021-06-10 21:13:00 -0500

    registroPacientes1.1

--- Database.php
+++ Database.php
@@ -3,13 +3,13 @@
 class Database extends PDO{
  
 	//dbname
-	private $dbname = 'ConsultorioD';
+	private $dbname = 'postgres';
 	//host
 	private $host 	= 'localhost';
 	//user database
 	private $user 	= 'postgres';
 	//password user
-	private $pass 	= 'dylan';
+	private $pass 	= '';
 	//port
 	private $port 	= 5432;
     //instance
